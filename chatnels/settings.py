200b03ce54e29ce5a3198569ebc9c372a678392f
arhantbararia <arhantbararia@gmail.com>
2025-01-24 02:42:32 +0530

    implemented channel layer

--- chatnels/settings.py
+++ chatnels/settings.py
@@ -134,3 +134,13 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 #daphne
 ASGI_APPLICATION = 'chatnels.asgi.application'
+
+##channels
+CHANNEL_LAYERS = {
+    "default": {
+        "BACKEND" : "channels_redis.core.RedisChannelLayer",
+        "CONFIG" : {
+            "hosts" : [("127.0.0.1" , "6379")],
+        },
+    },
+}
